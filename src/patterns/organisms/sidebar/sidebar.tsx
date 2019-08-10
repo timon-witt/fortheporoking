607bdab3bf15d58c252035c88c7f3b162ace29f8
Timon Witt <timonwitt@Timons-MacBook-Pro.local>
2019-08-10 23:08:50 +0200

    Add github link to sidebar

--- src/patterns/organisms/sidebar/sidebar.tsx
+++ src/patterns/organisms/sidebar/sidebar.tsx
@@ -15,7 +15,7 @@ export const Sidebar: React.FC<SidebarProps> = (_props: SidebarProps) => {
           Get a random champion
         </div>
       </div>
-      <div className="Sidebar-section">
+      <div className="Sidebar-primary">
         <NavLink exact activeClassName="is-active" to="/" className="Sidebar-button Sidebar-primaryButton">Any Champion</NavLink>
         <NavLink activeClassName="is-active" to="/fighter" className="Sidebar-button Sidebar-primaryButton">Fighter</NavLink>
         <NavLink activeClassName="is-active" to="/tank" className="Sidebar-button Sidebar-primaryButton">Tank</NavLink>
@@ -24,8 +24,9 @@ export const Sidebar: React.FC<SidebarProps> = (_props: SidebarProps) => {
         <NavLink activeClassName="is-active" to="/support" className="Sidebar-button Sidebar-primaryButton">Support</NavLink>
         <NavLink activeClassName="is-active" to="/marksman" className="Sidebar-button Sidebar-primaryButton">Marksman</NavLink>
       </div>
-      <div className="Sidebar-section">
+      <div className="Sidebar-secondary">
         <Link to="/impressum" className="Sidebar-button Sidebar-secondaryButton">Impressum</Link>
+        <a href="https://github.com/timon-witt/trollpick" className="Sidebar-button Sidebar-secondaryButton">GitHub</a>
       </div>
     </div>
   );
